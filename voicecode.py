b8786341c35db1ca407f2f46d588023d32a18101
Jensen Dempsey <jdemp@jensendempsey.com>
2015-10-23 18:07:24 -0500

    Initial commit

---
+++ voicecode.py
@@ -0,0 +1,14 @@
+__author__ = 'jdemp'
+
+class CodeDestination():
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def write_code_snippet(self, snippet):
+        pass
+
+
